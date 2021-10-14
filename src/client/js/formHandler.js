cc0530afc84338cc0d0ef6250be68ec8053bebc3
tunics <tunics@gmail.com>
2021-10-14 00:59:43 -0300

    Getting data from api

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,53 @@
+const baseURL = "https://api.meaningcloud.com/sentiment-2.1";
+const apiKey = "c9f5695f138d2189d763edca7ccee474";
+const lang = "en";
+const url =
+    "https://www.androidauthority.com/samsung-galaxy-s21-ultra-vs-iphone-13-pro-max-camera-test-3034358/"; //apagar
+
+// Async POST
+const postData = async (url = "", data = {}) => {
+    const response = await fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log("error", error);
+    }
+};
+
+function getAnalysis(url, key, lang) {
+    const formdata = new FormData();
+    formdata.append("key", key);
+    formdata.append("url", url);
+    formdata.append("lang", lang); // 2-letter code, like en es fr ...
+
+    const requestOptions = {
+        method: "POST",
+        body: formdata,
+        redirect: "follow",
+    };
+
+    const response = fetch(
+        "https://api.meaningcloud.com/sentiment-2.1",
+        requestOptions
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            return data;
+        })
+
+        .catch((error) => console.log("error", error));
+}
+
 function handleSubmit(event) {
     event.preventDefault();
 
@@ -5,8 +55,14 @@ function handleSubmit(event) {
     let formText = document.getElementById("name").value;
     Client.checkForName(formText);
 
+    getAnalysis(formText, apiKey, lang);
+
+    postData("http://localhost:8081/addEntry", {
+        message: formText,
+    });
+
     console.log("::: Form Submitted :::");
-    fetch("http://localhost:8081/test")
+    fetch("http://localhost:8081/addEntry")
         .then((res) => res.json())
         .then(function (res) {
             document.getElementById("results").innerHTML = res.message;
--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
 var path = require("path");
 const express = require("express");
 const mockAPIResponse = require("./mockAPI.js");
@@ -5,6 +8,17 @@ const mockAPIResponse = require("./mockAPI.js");
 const app = express();
 
 app.use(express.static("dist"));
+
+/* Dependencies */
+const bodyParser = require("body-parser");
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 console.log(__dirname);
 
@@ -20,4 +34,19 @@ app.listen(8081, function () {
 
 app.get("/test", function (req, res) {
     res.send(mockAPIResponse);
+});
+
+app.post("/addEntry", addEntry);
+
+function addEntry(req, res) {
+    newEntry = {
+        message: req.body.message,
+    };
+    projectData = newEntry;
+    console.log("posting data: " + projectData);
+}
+
+app.get("/addEntry", function (req, res) {
+    res.send(projectData);
+    console.log("sending data: " + projectData);
 });
